723181fd20289820f310cdf0fe63cb5e46f1fd8f
Ziyu Shi <57038180+JackShi148@users.noreply.github.com>
2023-03-08 11:21:10 -0500

    Add files via upload

---
+++ hwk4/main.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <fstream>
+#include <pqxx/pqxx>
+
+#include "exerciser.h"
+
+using namespace std;
+using namespace pqxx;
+
+void executeSQL(const string& sql, connection* C) {
+  work curTrans(*C);
+  curTrans.exec(sql);
+  curTrans.commit();
+}
+
+void dropTable(const string& table_name, connection* C) {
+  string sql;
+  sql = "DROP TABLE IF EXISTS " + table_name + " CASCADE;";
+  executeSQL(sql, C);
+}
+
+void createTable(const string& file_name, connection *C) {
+  string sql;
+  ifstream ifs(file_name.c_str(), ifstream::in);
+  string line;
+  while(getline(ifs, line)) {
+    sql += line;
+  }
+  ifs.close();
+  executeSQL(sql, C);
+}
+
+void addColorValue(const string& file_name, connection* C) {
+  int color_id;
+  string color_name, line;
+  ifstream ifs(file_name.c_str(), ifstream::in);
+  while(getline(ifs, line)) {
+    stringstream spliter;
+    spliter << line;
+    spliter >> color_id >> color_name;
+    add_color(C, color_name);
+  }
+  ifs.close();
+}
+
+void addStateValue(const string& file_name, connection* C) {
+  int state_id;
+  string state_name, line;
+  ifstream ifs(file_name.c_str(), ifstream::in);
+  while(getline(ifs, line)) {
+    stringstream spliter;
+    spliter << line;
+    spliter >> state_id >> state_name;
+    add_state(C, state_name);
+  }
+  ifs.close();
+}
+
+void addTeamValue(const string& file_name, connection* C) {
+  int team_id, state_id, color_id;
+  int wins, losses;
+  string team_name, line;
+  ifstream ifs(file_name.c_str(), ifstream::in);
+  while(getline(ifs, line)) {
+    stringstream spliter;
+    spliter << line;
+    spliter >> team_id >> team_name >> state_id >> color_id >> wins >> losses;
+    add_team(C, team_name, state_id, color_id, wins, losses);
+  }
+  ifs.close();
+}
+
+void addPlayerValue(const string& file_name, connection* C) {
+  int player_id, team_id, jersey_num;
+  int mpg, ppg, rpg, apg;
+  double spg, bpg;
+  string first_name, last_name, line;
+  ifstream ifs(file_name.c_str(), ifstream::in);
+  while(getline(ifs, line)) {
+    stringstream spliter;
+    spliter << line;
+    spliter >> player_id >> team_id >> jersey_num >> first_name >> last_name
+            >> mpg >> ppg >> rpg >> apg >> spg >> bpg;
+    add_player(C, team_id, jersey_num, first_name, last_name, mpg, ppg, rpg, apg, spg, bpg);
+  }
+  ifs.close();
+}
+
+int main (int argc, char *argv[]) 
+{
+
+  //Allocate & initialize a Postgres connection object
+  connection *C;
+
+  try{
+    //Establish a connection to the database
+    //Parameters: database name, user name, user password
+    C = new connection("dbname=ACC_BBALL user=postgres password=passw0rd");
+    if (C->is_open()) {
+      cout << "Opened database successfully: " << C->dbname() << endl;
+    } else {
+      cout << "Can't open database" << endl;
+      return 1;
+    }
+  } catch (const std::exception &e){
+    cerr << e.what() << std::endl;
+    return 1;
+  }
+
+
+  //TODO: create PLAYER, TEAM, STATE, and COLOR tables in the ACC_BBALL database
+  //      load each table with rows from the provided source txt files
+  try {
+    dropTable("PLAYER", C);
+    dropTable("TEAM", C);
+    dropTable("STATE", C);
+    dropTable("COLOR", C);
+    createTable("create_table.sql", C);
+    addColorValue("color.txt", C);
+    addStateValue("state.txt", C);
+    addTeamValue("team.txt", C);
+    addPlayerValue("player.txt", C);
+  }
+  catch(const exception& e) {
+    cerr << e.what() << endl;
+    return 1;
+  }
+
+  exercise(C);
+
+
+  //Close database connection
+  C->disconnect();
+
+  return 0;
+}
+
+
